b829d34d3f1d279023eadf63cea7fb45790836df
fokosun <okosunuzflorence@gmail.com>
2020-08-22 12:23:31 -0400

    WIP: FWF compoenent

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ui grid" id="app">
+	  <div class="six wide column">
+		  <FunWithFlagsComponent :flags="flags" />
+	  </div>
+  </div>
+</template>
+
+<script>
+import FunWithFlagsComponent from './FunWithFlagsComponent.vue'
+import flags from './flags.js'
+
+export default {
+	name: 'app',
+	computed: {
+		flags() {
+			let data = Object.values(flags.data)
+			return data
+		}
+	},
+	data () {
+		return {}
+	},
+	components: {
+		FunWithFlagsComponent
+	}
+}
+</script>
---
+++ src/FunWithFlagsComponent.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="ui grid">
+    <div class="three wide column">
+        {{ nationality }}
+     </div>
+     <div class="three wide column">
+        <select class="ui dropdown">
+            <option value="1" v-for="flagObject in flags">
+                {{ flagObject.code }}
+            </option>
+        </select>
+     </div>
+     <div class="three wide column">
+         <i class=""></i>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'FunWithFlagsCompoenent',
+    props: {
+        flags: Array
+    },
+    data () {
+        return {
+            nationality: '',
+            _class: ''
+        }
+    }
+}
+</script>
